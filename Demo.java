4206d02063e43134b6e57407b0e40c031fb6fe24
NagarajuJN <nagaraju1998jn@gmail.com>
2023-06-24 21:18:20 +0530

    hi is added

--- Demo.java
+++ Demo.java
@@ -4,9 +4,7 @@ class Demo
 {
 	public static void main(String[] args) 
 	{
-		Scanner scan = new Scanner(System.in);
-		String s = scan.next();
-		System.out.println(s.length());
+		System.out.println("Hii");
 
 
 		
--- P26.java
+++
@@ -1,6 +0,0 @@
-Class P26{
-	public static void main(String[] args) {
-	String s = "Where";
-	StringBuffer sb = new StringBuffer("Where");
-	System.out.print(sb.equals(s) +","+s.equals(sb));
-}}
