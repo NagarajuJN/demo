76435ee7e9d11156128d3b18ad2717953a5012ce
NagarajuJN <nagaraju1998jn@gmail.com>
2023-06-24 21:38:43 +0530

    while loop is added

--- Numbers.java
+++ Numbers.java
@@ -3,9 +3,11 @@ class Numbers
 	public static void main(String[] args) 
 	{
 
-		for(int i = 0 ; i<= 10 ; i++){
+		int i = 10;
+		while (i<=0)
 
-		System.out.println(i);
-	}
+		{
+			System.out.println(i);
+		}
 	}
 }
